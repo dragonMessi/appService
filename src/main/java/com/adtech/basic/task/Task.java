abb94e8430f8551854d6fc20eba2376ea891ad99
thinkpad <306971926@qq.com>
2019-05-30 14:43:51 +0800

    改参数1

--- src/main/java/com/adtech/basic/task/Task.java
+++ src/main/java/com/adtech/basic/task/Task.java
@@ -16,86 +16,86 @@ public class Task {
     private static final String urlOne = "http://127.0.0.1:80/ios/restFull/";
     private static final String urlTwo = "http://127.0.0.1:80/ios/restFulVersionTwo/";
 
-
-    //vip执行第三方订单 vipThirdOrderPerform
-    @Scheduled(cron = "0 0/10 * * * ? ")//10分钟都执行
-    @Async
-    public void vipThirdOrderPerform() {
-
-        try {
-            String result = HttpClientUtil.doget(urlOne + "vipThirdOrderPerform.do");
-
-            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip执行第三方订单 vipThirdOrderPerform");
-        } catch (Exception e) {
-            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip执行第三方订单 vipThirdOrderPerform报错");
-        }
-
-    }
-
-
-    //vip执行第三方订单状态查询 vipThirdOrderStatus
-    @Scheduled(cron = "0 0/30 * * * ? ")//10分钟都执行
-    @Async
-    public void vipThirdOrderStatus() {
-        try {
-            String result = HttpClientUtil.doget(urlOne + "vipThirdOrderStatus.do");
-            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip执行第三方订单状态查询vipThirdOrderStatus");
-        } catch (Exception e) {
-            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip执行第三方订单状态查询vipThirdOrderStatus报错");
-        }
-    }
-
-
-    //vip订单每天自动添加 autoVipThirdOrder
-    @Scheduled(cron = "0 0/30 * * * ? ")//10分钟都执行
-    @Async
-    public void autoVipThirdOrder() {
-        try {
-            String result = HttpClientUtil.doget(urlOne + "autoVipThirdOrder.do");
-            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip订单每天自动添加 autoVipThirdOrder");
-
-        } catch (Exception e) {
-            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip订单每天自动添加 autoVipThirdOrder报错");
-        }
-
-    }
-
-
-    //自动添加VIP第三方订单第二版
-    @Scheduled(cron = "0 0/30 * * * ? ")//5分钟都执行
-    @Async
-    public void autoVipThirdOrderTwo() {
-        try {
-            String result = HttpClientUtil.doget(urlTwo + "autoVipThirdOrderTwo.do");
-            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "自动添加VIP第三方订单第二版autoVipThirdOrderTwo");
-
-        } catch (Exception e) {
-            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "自动添加VIP第三方订单第二版autoVipThirdOrderTwo报错");
-        }
-    }
-
-    //vip第三方订单添状态查询第二版
-    @Scheduled(cron = "0 0/20 * * * ? ")//5分钟都执行
-    @Async
-    public void vipThirdOrderStatusTwo() {
-        try {
-            String result = HttpClientUtil.doget(urlTwo + "vipThirdOrderStatusTwo.do");
-            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip第三方订单添状态查询第二版vipThirdOrderStatusTwo");
-        } catch (Exception e) {
-            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip第三方订单添状态查询第二版vipThirdOrderStatusTwo报错");
-        }
-    }
-
-    //vip第三方订单执行第二版
-    @Scheduled(cron = "0 0/60 * * * ? ")//5分钟都执行
-    @Async
-    public void vipThirdOrderPerformTwo() {
-        try {    String result = HttpClientUtil.doget(urlTwo + "vipThirdOrderPerformTwo.do");
-            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip第三方订单执行第二版vipThirdOrderPerformTwo");
-
-        } catch (Exception e) {
-            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip第三方订单执行第二版vipThirdOrderPerformTwo报错");
-        }
-    }
+//
+//    //vip执行第三方订单 vipThirdOrderPerform
+//    @Scheduled(cron = "0 0/10 * * * ? ")//10分钟都执行
+//    @Async
+//    public void vipThirdOrderPerform() {
+//
+//        try {
+//            String result = HttpClientUtil.doget(urlOne + "vipThirdOrderPerform.do");
+//
+//            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip执行第三方订单 vipThirdOrderPerform");
+//        } catch (Exception e) {
+//            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip执行第三方订单 vipThirdOrderPerform报错");
+//        }
+//
+//    }
+//
+//
+//    //vip执行第三方订单状态查询 vipThirdOrderStatus
+//    @Scheduled(cron = "0 0/30 * * * ? ")//10分钟都执行
+//    @Async
+//    public void vipThirdOrderStatus() {
+//        try {
+//            String result = HttpClientUtil.doget(urlOne + "vipThirdOrderStatus.do");
+//            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip执行第三方订单状态查询vipThirdOrderStatus");
+//        } catch (Exception e) {
+//            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip执行第三方订单状态查询vipThirdOrderStatus报错");
+//        }
+//    }
+//
+//
+//    //vip订单每天自动添加 autoVipThirdOrder
+//    @Scheduled(cron = "0 0/30 * * * ? ")//10分钟都执行
+//    @Async
+//    public void autoVipThirdOrder() {
+//        try {
+//            String result = HttpClientUtil.doget(urlOne + "autoVipThirdOrder.do");
+//            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip订单每天自动添加 autoVipThirdOrder");
+//
+//        } catch (Exception e) {
+//            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip订单每天自动添加 autoVipThirdOrder报错");
+//        }
+//
+//    }
+//
+//
+//    //自动添加VIP第三方订单第二版
+//    @Scheduled(cron = "0 0/30 * * * ? ")//5分钟都执行
+//    @Async
+//    public void autoVipThirdOrderTwo() {
+//        try {
+//            String result = HttpClientUtil.doget(urlTwo + "autoVipThirdOrderTwo.do");
+//            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "自动添加VIP第三方订单第二版autoVipThirdOrderTwo");
+//
+//        } catch (Exception e) {
+//            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "自动添加VIP第三方订单第二版autoVipThirdOrderTwo报错");
+//        }
+//    }
+//
+//    //vip第三方订单添状态查询第二版
+//    @Scheduled(cron = "0 0/20 * * * ? ")//5分钟都执行
+//    @Async
+//    public void vipThirdOrderStatusTwo() {
+//        try {
+//            String result = HttpClientUtil.doget(urlTwo + "vipThirdOrderStatusTwo.do");
+//            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip第三方订单添状态查询第二版vipThirdOrderStatusTwo");
+//        } catch (Exception e) {
+//            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip第三方订单添状态查询第二版vipThirdOrderStatusTwo报错");
+//        }
+//    }
+//
+//    //vip第三方订单执行第二版
+//    @Scheduled(cron = "0 0/60 * * * ? ")//5分钟都执行
+//    @Async
+//    public void vipThirdOrderPerformTwo() {
+//        try {    String result = HttpClientUtil.doget(urlTwo + "vipThirdOrderPerformTwo.do");
+//            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip第三方订单执行第二版vipThirdOrderPerformTwo");
+//
+//        } catch (Exception e) {
+//            System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "vip第三方订单执行第二版vipThirdOrderPerformTwo报错");
+//        }
+//    }
 
 }
